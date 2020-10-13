bd8f0db0d52b8596108a44ea8aa70a2279b64539
adrianjost <22987140+adrianjost@users.noreply.github.com>
2020-10-13 21:43:20 +0200

    fine tune button control and apply realistic time values

--- SmartLight-Firmware/SmartLight-Firmware.ino
+++ SmartLight-Firmware/SmartLight-Firmware.ino
@@ -73,11 +73,11 @@ Date: 7 June 2020
 #define BRIGHTNESS_MIN 0
 #define BRIGHTNESS_MAX 255
 #define BRIGHNESS_STEP 1
-#define BRIGHNESS_STEP_DURATION 40
+#define BRIGHNESS_STEP_DURATION 60
 #define HUE_MIN 0
 #define HUE_MAX 1
 #define HUE_STEP 0.01
-#define HUE_STEP_DURATION 50
+#define HUE_STEP_DURATION 120
 
 // Durations in ms
 #define DURATION_MINUTE 60000
@@ -841,8 +841,8 @@ void handleButton(){
 // time control
 //*************************
 // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23
-byte time_brightness[24] = {55,55,55,55,55,55,55,55,55,55,55,55,55,55,55,55,55,55,55,55,55,55,55,55};
-float time_hue[24] = {0.0, 0.5, 1.0, 0.0, 0.5, 1.0, 0.0, 0.5, 1.0, 0.0, 0.5, 1.0, 0.0, 0.5, 1.0, 0.0, 0.5, 1.0, 0.0, 0.5, 1.0, 0.0, 0.5, 1.0};
+byte time_brightness[24] = {1, 1, 3, 5, 13, 51, 128, 204, 230, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 204, 128, 45, 10, 1, 1};
+float time_hue[24] = {0.01, 0.01, 0.01, 0.02, 0.05, 0.2, 0.5, 0.8, 0.9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0.8, 0.5, 0.2, 0.05, 0.01, 0.01};
 
 #define STEP_PRECISION 10000.0
 
