ee9ba8281a626b2562755ce953e28d038ef7d555
adrianjost <22987140+adrianjost@users.noreply.github.com>
2020-10-25 12:00:18 +0100

    simplify firmware to only support WWCW Lights

--- SmartLight-Firmware/SmartLight-Firmware.ino
+++ SmartLight-Firmware/SmartLight-Firmware.ino
@@ -1,21 +1,14 @@
 /**
 Author: Adrian Jost
-Version: 3.1.0
+Version: 4.0.0
 Date: 7 June 2020
 **/
 
 // PIN DEFINITIONS
 #define PIN_RESET 0 // comment out on boards without FLASH-button
 #define PIN_INPUT 2
-// TODO: remove all NEO-Pixel code if not defined
-// #define PIN_NEO 1
 #define PIN_CH1 1
-// #define PIN_CH2 3
-#define PIN_CH3 3
-
-// NeoPixel settings
-#define NEO_BRIGHTNESS 100
-#define NEO_PIXELS 100
+#define PIN_CH2 3
 
 // config storage
 #define PATH_CONFIG_WIFI "/config.json"
@@ -26,21 +19,15 @@ Date: 7 June 2020
 #define JSON_TIME_BRIGHTNESS "tb"
 #define JSON_TIME_HUE "th"
 
+// Debug colors
+#define OFF Channels{0,0}
+#define ON Channels{20,20}
+
 // current state
 #define STATE_OFF 0
 #define STATE_UNDEFINED 1
 #define STATE_COLOR 2
-#define STATE_GRADIENT 3
-#define STATE_TIME 4
-
-// Debug colors
-#define BLACK RGB{0,0,0}
-#define WHITE RGB{20,20,20}
-#define RED RGB{20,0,0}
-#define GREEN RGB{0,20,0}
-#define BLUE RGB{0,0,20}
-#define ORANGE RGB{20,20,0}
-#define VIOLET RGB{20,0,20}
+#define STATE_TIME 3
 
 // button control
 #define TIMEOUT 500
@@ -68,15 +55,8 @@ Date: 7 June 2020
 #include <ArduinoJson.h>        // 6.16.1 - Benoit Blanchon
 #include <WebSocketsServer.h>   // 2.1.4 Markus Sattler
 
-#include <ESP8266WebServer.h>   //
-
 // OTA Updates
 #include <ArduinoOTA.h>
-
-// LED Strips
-#ifdef PIN_NEO
-#include <Adafruit_NeoPixel.h>  // 1.3.1 Adafruit
-#endif
 
 // Time Keeping
 #include <NTPClient.h> // 3.2.0 Fabrice Weinberg
@@ -88,69 +68,9 @@ Date: 7 June 2020
   #define DEBUG_SPEED 115200
 #endif
 
-/*
-API
-===============================================================================
-<color values are from 0-255>
-Examples:
-# 1 set color
-send:
-  {
-    color: {
-      1: 255,
-      2: 100,
-      3: 0
-    }
-  }
-
-# 2 set gradient
-send:
-  {
-    gradient: {
-      colors: [
-        {
-          1: 255,
-          2: 0,
-          3: 0
-        },
-        {
-          1: 0,
-          2: 0,
-          3: 255
-        },
-        {
-          1: 255,
-          2: 0,
-          3: 0
-        }
-      ],
-      transitionTimes: [0, 10000, 20000], // check 2.1
-      loop: true // check 2.2
-    }
-  }
-
-## 2.1 transitionTimes
-a colorTimestamp n declars when the nth color should be fully visible in milliseconds (1s = 1000ms).
-In the given example, we start with a red color at 0s and transition smoothly to blue.
-After 10s we are showing a solid blue color and begin fading to the 3th color (red).
-After 10 more seconds (20s in total) we are showing full red again.
-! the first value must be 0 and the total number of transitionTimes must be equal to the number of gradient colors. !
-
-## 2.2 loop
-the loop option declares wheather the gradient should start again when reaching the last color.
-We do not do any type of fading here!
-To make this transition smooth your gradient should start and end with the same colors.
-===============================================================================
-*/
-
 WiFiManager wm;
 
-#ifdef PIN_NEO
-  Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NEO_PIXELS, PIN_NEO, NEO_GRB + NEO_KHZ800);
-#endif
-
 FS* filesystem = &LittleFS;
-ESP8266WebServer server(81);
 WebSocketsServer webSocket = WebSocketsServer(80);
 
 // Define NTP Client to get time
@@ -158,41 +78,29 @@ WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, "pool.ntp.org", 0);
 
 // JSON sizes https://arduinojson.org/v6/assistant/
-// max 25 gradient steps, 2226 Bytes, each timestamp max 7 digits, each color val max 255
-const size_t maxPayloadSize = 2*JSON_ARRAY_SIZE(25) + 26*JSON_OBJECT_SIZE(3) + 200;
-/*
-{
-  "JSON_HOSTNAME": "abcdef",
-  "JSON_LAMP_TYPE": "test"
-}
-*/
+// { "color": { "1": 255, "2": 255, "3": 255 } }
+const size_t maxPayloadSize = JSON_OBJECT_SIZE(1) + JSON_OBJECT_SIZE(3) + 20;
+
+// { "JSON_HOSTNAME": "abcdef", "JSON_LAMP_TYPE": "test" }
 const size_t maxWifiConfigSize = JSON_OBJECT_SIZE(2) + 80;
-/*
-{
-  "JSON_TIME_BRIGHTNESS": 24[255],
-  "JSON_TIME_HUE": 24[100]
-}
-*/
+
+// { "JSON_TIME_BRIGHTNESS": 24[255], "JSON_TIME_HUE": 24[100] }
 const size_t maxTimeConfigSize = 2*JSON_ARRAY_SIZE(24) + JSON_OBJECT_SIZE(2) + 30;
 
-struct RGB {
-  byte r;
-  byte g;
+struct Channels {
+  byte a;
   byte b;
 };
 
-struct floatRGB {
-  float r;
-  float g;
+struct FloatChannels {
+  float a;
   float b;
 };
-
 
 //*************************
 // global State
 //*************************
 
-char lamptype[32] = "Analog"; // "NeoPixel", "Analog"
 char hostname[32] = "A CHIP";
 char wifiPassword[32] = "";
 
@@ -201,18 +109,12 @@ byte currentState = STATE_OFF;
 bool hasNewValue = false;
 
 // currentState == STATE_COLOR
-RGB currentColor {0,0,0};
-
-// currentState == STATE_GRADIENT
-RGB *currentGradientColors; // create pointer where informations can be stored
-unsigned long *currentGradientTimes; // create pointer where informations can be stored
-byte numberOfSteps;
-bool currentGradientLoop;
+Channels currentOutput {0,0};
 
 // currentState == STATE_UNDEFINED || currentState == STATE_TIME
 byte brightness = 0;
 float hue = 0.5;
-#define TIMEZONE_OFFSET 2 // TODO: values should always be saved in UTC
+#define TIMEZONE_OFFSET 1 // TODO: values should always be saved in UTC
 // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23
 byte time_brightness[24] = {1, 1, 3, 5, 13, 51, 128, 204, 230, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 130, 40, 20, 5, 1};
 float time_hue[24] = {0.01, 0.01, 0.01, 0.02, 0.05, 0.2, 0.5, 0.8, 0.9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0.8, 0.5, 0.3, 0.1, 0.05, 0.01};
@@ -221,89 +123,47 @@ float time_hue[24] = {0.01, 0.01, 0.01, 0.02, 0.05, 0.2, 0.5, 0.8, 0.9, 1, 1, 1,
  INIT
 **********************************/
 
-void initStripNeoPixel(){
-  #ifdef PIN_NEO
-    pixels.begin(); // This initializes the NeoPixel library.
-    pixels.setBrightness(NEO_BRIGHTNESS);
-    pixels.show();
+void initStrip(){
+  #ifdef DEBUG
+    Serial.println("initStrip");
   #endif
-}
-void initStripAnalog(){
+
   #ifdef PIN_CH1
     pinMode(PIN_CH1, OUTPUT);
   #endif
   #ifdef PIN_CH2
     pinMode(PIN_CH2, OUTPUT);
   #endif
-  #ifdef PIN_CH3
-    pinMode(PIN_CH3, OUTPUT);
-  #endif
-}
-void initStrip(){
-  #ifdef DEBUG
-    Serial.println("initStrip");
-    Serial.println(String(lamptype));
-    return;
-  #endif
-
-  // set new color
-  // "NeoPixel", "Analog"
-  if(String(lamptype) == String("Analog")){
-    initStripAnalog();
-  }else{
-    initStripNeoPixel();
-  }
 }
 
 /**********************************
  SET COLOR
 **********************************/
 
-void setColorNeoPixel(RGB color){
-  #ifdef PIN_NEO
-    for (int i = 0; i < pixels.numPixels(); i++) {
-      pixels.setPixelColor(i, pixels.Color(color.r, color.g, color.b));
-    }
-    pixels.show();
-  #endif
-}
-void setColorAnalog(RGB color){
-  #ifdef PIN_CH1
-    analogWrite(PIN_CH1, map(color.r,0,255,0,1024));
-  #endif
-  #ifdef PIN_CH2
-    analogWrite(PIN_CH2, map(color.g,0,255,0,1024));
-  #endif
-  #ifdef PIN_CH3
-    analogWrite(PIN_CH3, map(color.b,0,255,0,1024));
-  #endif
-}
-void setColor(RGB color){
+void setOutput(Channels ch){
   #ifdef DEBUG
-    Serial.print("setColor: ");
-    Serial.print(color.r);
+    Serial.print("setOutput: ");
+    Serial.print(ch.a);
     Serial.print(", ");
-    Serial.print(color.g);
-    Serial.print(", ");
-    Serial.print(color.b);
+    Serial.print(ch.b);
     Serial.println("");
     return;
   #endif
 
-  // set new color
-  // "NeoPixel", "Analog"
-  if(String(lamptype) == String("Analog")){
-    setColorAnalog(color);
-  }else{
-    setColorNeoPixel(color);
-  }
+  #ifdef PIN_CH1
+    analogWrite(PIN_CH1, map(ch.a,0,255,0,1024));
+  #endif
+  #ifdef PIN_CH2
+    analogWrite(PIN_CH2, map(ch.b,0,255,0,1024));
+  #endif
 }
-void blink(RGB color, int speed){
-  while(true){
-    setColor(color);
-    delay(speed);
-    setColor(BLACK);
-    delay(speed);
+
+void blink(Channels ch, byte num, int blinkDuration){
+  for (byte i = 0; i <= num; i++) {
+    setOutput(ch);
+    delay(blinkDuration);
+    setOutput(OFF);
+    delay(blinkDuration);
   }
 }
 
@@ -327,7 +187,7 @@ void configModeCallback (WiFiManager *myWiFiManager) {
   #ifdef DEBUG
     Serial.println("start config portal");
   #endif
-  setColor(VIOLET);
+  blink(ON, 3, 200);
 }
 
 void setupWifiConfig() {
@@ -341,8 +201,6 @@ void setupWifiConfig() {
   #ifdef DEBUG
     Serial.print("hostname: ");
     Serial.println(hostname);
-    Serial.print("lamptype: ");
-    Serial.println(lamptype);
   #endif
 
   #ifdef DEBUG
@@ -391,15 +249,6 @@ void setupWifiConfig() {
     strcpy(hostname, doc[JSON_HOSTNAME]);
     #ifdef DEBUG
       Serial.println("hostname updated");
-    #endif
-  }
-  if(doc.containsKey(JSON_LAMP_TYPE)){
-    #ifdef DEBUG
-      Serial.println("JSON_LAMP_TYPE key read");
-    #endif
-    strcpy(lamptype, doc[JSON_LAMP_TYPE]);
-    #ifdef DEBUG
-      Serial.println("lamptype updated");
     #endif
   }
 }
@@ -495,7 +344,6 @@ bool shouldEnterSetup(){
       if(buttonState == LOW){
         click_count++;
         if(click_count >= clickThreshould){
-          setColor(VIOLET);
           pinMode(PIN_RESET, OUTPUT);
           digitalWrite(PIN_RESET, HIGH);
           return true;
@@ -523,11 +371,7 @@ void setupWifi(){
   wm.setHostname(hostname);
 
   WiFiManagerParameter setting_hostname(JSON_HOSTNAME, "Devicename: (e.g. <code>smartlight-kitchen</code>)", hostname, 32);
-  WiFiManagerParameter setting_lamptype(JSON_LAMP_TYPE, "Type of connected lamp:<br /><span>Options: <code>NeoPixel</code>, <code>Analog</code></span>", lamptype, 32);
   wm.addParameter(&setting_hostname);
-  wm.addParameter(&setting_lamptype);
-
-  setColor(GREEN);
 
   bool forceSetup = shouldEnterSetup();
   bool setup = forceSetup
@@ -541,7 +385,6 @@ void setupWifi(){
     DynamicJsonDocument doc(maxWifiConfigSize);
 
     doc[JSON_HOSTNAME] = setting_hostname.getValue();
-    doc[JSON_LAMP_TYPE] = setting_lamptype.getValue();
 
     File configFile = filesystem->open(PATH_CONFIG_WIFI, "w");
     serializeJson(doc, configFile);
@@ -551,14 +394,11 @@ void setupWifi(){
       Serial.println("config written to filesystem");
     #endif
 
-    setColor(GREEN);
-    delay(1000);
-    setColor(BLACK);
     ESP.restart();
   }
 
   if(!setup){
-    setColor(RED);
+    blink(ON, 10, 200);
     // shut down till the next reboot
     // ESP.deepSleep(86400000000); // 1 Day
     ESP.deepSleep(300000000); // 5 Minutes
@@ -603,140 +443,11 @@ void setupOTAUpdate(){
 }
 
 //*************************
-// gradient stuff
-//*************************
-bool gradientActive = false;
-unsigned long stepStartTime;
-unsigned long lastStepTime;
-unsigned long targetTime;
-unsigned long currentStepDuration;
-floatRGB currentGradientColor;
-floatRGB dy;
-byte gradientState;
-
-RGB floatRGBtoRGB(floatRGB color){
-  return RGB {
-    (byte)color.r,
-    (byte)color.g,
-    (byte)color.b
-  };
-}
-
-floatRGB RGBtofloatRGB(RGB color){
-  return floatRGB {
-    (float)color.r,
-    (float)color.g,
-    (float)color.b
-  };
-}
-
-float gradientCalculateNewColorChannelValue(unsigned long dx, float dy, float color){
-  return color + (dx * dy);
-}
-
-floatRGB gradientCalculateNewColor(unsigned long dx, floatRGB dy, floatRGB color){
-  color = floatRGB {
-    gradientCalculateNewColorChannelValue(dx, dy.r, color.r),
-    gradientCalculateNewColorChannelValue(dx, dy.g, color.g),
-    gradientCalculateNewColorChannelValue(dx, dy.b, color.b),
-  };
-  currentColor = floatRGBtoRGB(color);
-  setColor(currentColor);
-  return color;
-}
-
-//void fade(uint8_t num, unsigned long duration, RGB before, RGB after){
-void gradientInitFade(unsigned long duration, RGB before, RGB after){
-  stepStartTime = millis();
-  currentStepDuration = duration;
-  lastStepTime = stepStartTime;
-
-  dy = (floatRGB){
-    (float)((after.r - before.r) / (float)duration),
-    (float)((after.g - before.g) / (float)duration),
-    (float)((after.b - before.b) / (float)duration)
-  };
-
-  currentGradientColor = RGBtofloatRGB(before);
-
-  gradientActive = true;
-}
-
-void gradientStep(){
-  // breakpoint not reached -> continue fading
-  unsigned long currentTime = millis();
-  if((unsigned long)(currentTime - stepStartTime) < currentStepDuration) {
-    unsigned long dx = currentTime - lastStepTime;
-    lastStepTime = currentTime;
-    currentGradientColor = gradientCalculateNewColor(dx, dy, currentGradientColor);
-    return;
-  }
-  // transition done
-  if(gradientState < (numberOfSteps - 1)){ // more transitions defined?
-    // calculate next step
-    gradientState++;
-    RGB startColor = currentGradientColors[gradientState - 1];
-    RGB targetColor = currentGradientColors[gradientState];
-    unsigned long stopTimeStart = currentGradientTimes[gradientState - 1];
-    unsigned long stopTimeTarget = currentGradientTimes[gradientState];
-    unsigned long duration = stopTimeTarget - stopTimeStart;
-    gradientInitFade(duration, startColor, targetColor);
-    return;
-  }
-  // gradient should loop?
-  if(currentGradientLoop){
-    // restart gradient
-    gradientState = 0;
-    return;
-  }
-  // stop gradient
-  gradientActive = false;
-}
-
-bool gradientLoop(bool restart){
-  if(restart){
-    gradientState = 0;
-    gradientActive = true;
-  }
-  if(gradientActive){
-    gradientStep();
-    return true;
-  }else{
-    return false;
-  }
-}
-
-//*************************
-// webserver communication
-//*************************
-
-void handleRoot() {
-  server.send(200, "text/plain", "Available routes:\nGET /color");   // Send HTTP status 200 (Ok) and send some text to the browser/client
-}
-
-void handleNotFound(){
-  server.send(404, "text/plain", "404: Not found"); // Send HTTP status 404 (Not Found) when there's no handler for the URI in the request
-}
-
-void handleColor(){
-    server.send(200, "text/json", "{\"color\":{\"1\":\"" + String(currentColor.r) + "\",\"2\":\"" + String(currentColor.g) + "\",\"3\":\"" + String(currentColor.b) + "\"}}");
-}
-
-void setupServer(){
-  server.on("/", handleRoot);
-  server.on("/color", handleColor);
-  server.onNotFound(handleNotFound);
-
-  server.begin();
-}
-
-
-//*************************
 // websocket communication
 //*************************
 
 void broadcastCurrentColor() {
-  webSocket.broadcastTXT("{\"color\":{\"1\":" + String(currentColor.r) + ",\"2\":" + String(currentColor.g) + ",\"3\":" + String(currentColor.b) + "}}");
+  webSocket.broadcastTXT("{\"color\":{\"1\":" + String(currentOutput.a) + ",\"2\":" + String(currentOutput.b) + "}}");
 }
 
 void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght) {
@@ -757,42 +468,18 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t * payload, size_t lenght
 
         if (error) {
           // ERROR parsing State
-          currentState = STATE_UNDEFINED;
           webSocket.sendTXT(num, "{\"status\":\"Error\",\"data\":\"Failed to parse payload\"}");
           return;
         }
         if(tmpStateJson.containsKey("color")){
-          currentColor = {
+          currentOutput = {
             tmpStateJson["color"]["1"],
-            tmpStateJson["color"]["2"],
-            tmpStateJson["color"]["3"]
+            tmpStateJson["color"]["2"]
           };
           currentState = STATE_COLOR;
           hasNewValue = true;
           broadcastCurrentColor();
-        }else if(tmpStateJson.containsKey("gradient")){
-          gradientState = 0;
-          currentStepDuration = 0;
-          free(currentGradientColors);
-          free(currentGradientTimes);
-          numberOfSteps = tmpStateJson["gradient"]["colors"].size();
-          currentGradientColors = (RGB *) malloc(sizeof(RGB) * numberOfSteps);
-          currentGradientTimes = (unsigned long *) malloc(sizeof(unsigned long) * numberOfSteps);
-
-          currentGradientLoop = tmpStateJson["gradient"]["loop"];
-          for (int i = 0; i < numberOfSteps; i++) {
-            (currentGradientColors)[i] = RGB {
-              tmpStateJson["gradient"]["colors"][i]["1"],
-              tmpStateJson["gradient"]["colors"][i]["2"],
-              tmpStateJson["gradient"]["colors"][i]["3"]
-            };
-            (currentGradientTimes)[i] = tmpStateJson["gradient"]["transitionTimes"][i];
-          }
-          currentState = STATE_GRADIENT;
-          hasNewValue = true;
-          webSocket.sendTXT(num, "{\"status\":\"OK\"}");
         }else{
-          currentState = STATE_UNDEFINED;
           webSocket.sendTXT(num, "{\"status\":\"Error\",\"data\":\"Unknown Payload\"}");
         }
       }
@@ -809,15 +496,28 @@ void setupWebsocket(){
 // button control
 //*************************
 
+Channels floatChtoCh(FloatChannels color){
+  return Channels {
+    (byte)color.a,
+    (byte)color.b,
+  };
+}
+
+FloatChannels chtoFloatCh(Channels color){
+  return FloatChannels {
+    (float)color.a,
+    (float)color.b,
+  };
+}
+
 void updateLED() {
   float ww = hue < 0.5 ? 1 : (2 - (2 * hue));
   float cw = hue < 0.5 ? (hue * 2) : 1;
-  currentColor = floatRGBtoRGB({
+  currentOutput = floatChtoCh({
     constrain(brightness * ww, BRIGHTNESS_MIN, BRIGHTNESS_MAX),
-    0,
     constrain(brightness * cw, BRIGHTNESS_MIN, BRIGHTNESS_MAX)
   });
-  setColor(currentColor);
+  setOutput(currentOutput);
 }
 
 bool isBtn(bool state = true, unsigned long debounceDuration = 50) {
@@ -850,9 +550,9 @@ bool waitForBtn(int ms, bool state = true) {
   return false;
 }
 
-float getHue(RGB color) {
-  floatRGB c = RGBtofloatRGB(color);
-  float warm = c.r;
+float getHue(Channels ch) {
+  FloatChannels c = chtoFloatCh(ch);
+  float warm = c.a;
   float cold = c.b;
   if(warm == cold){
     return 0.5;
@@ -870,8 +570,8 @@ float getHue(RGB color) {
   }
 }
 
-float getBrightness(RGB color) {
-  return max(color.r, color.b);
+float getBrightness(Channels ch) {
+  return max(ch.a, ch.b);
 }
 
 void handleButton(){
@@ -989,16 +689,11 @@ void setup() {
 
   initStrip();
 
-  setColor(BLUE);
+  blink(ON, 1, 200);
   setupWifi();
-  setColor(GREEN);
+  blink(ON, 2, 200);
   setupOTAUpdate();
-  setColor(BLACK);
 
-  currentGradientColors = (RGB *) malloc(sizeof(RGB));
-  currentGradientTimes = (unsigned long *) malloc(sizeof(unsigned long));
-
-  setupServer();
   setupWebsocket();
 
   timeClient.begin();
@@ -1012,37 +707,20 @@ void setup() {
 void loop() {
   ArduinoOTA.handle(); // listen for OTA Updates
   webSocket.loop(); // listen for websocket events
-  server.handleClient(); // listen for HTTP Requests
   #ifdef PIN_INPUT
     handleButton(); // listen for hardware inputs
   #endif
   switch (currentState) {
     case STATE_COLOR: {
-        setColor(currentColor);
+        setOutput(currentOutput);
         hasNewValue = false;
         currentState = STATE_UNDEFINED;
-        brightness = getBrightness(currentColor);
-        hue = getHue(currentColor);
-      }
-      break;
-    case STATE_GRADIENT: {
-        if(!gradientLoop(hasNewValue)){
-          currentState = STATE_UNDEFINED;
-        }else{
-          hasNewValue = false;
-        }
-        brightness = getBrightness(currentColor);
-        hue = getHue(currentColor);
+        brightness = getBrightness(currentOutput);
+        hue = getHue(currentOutput);
       }
       break;
     case STATE_TIME: {
         setByTime();
-      }
-      break;
-    case STATE_UNDEFINED: {
-        if(currentColor.r == 0 && currentColor.g == 0&& currentColor.b == 0){
-          currentState = STATE_OFF;
-        }
       }
       break;
   }
